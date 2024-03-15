7a3b1865dc452104adda5109ab52e4a5b51e7083
phonepasit <jimmybleach@gmail.com>
2024-03-15 19:38:38 +0700

    admin migration

--- database/migrations/2024_03_12_144820_create_admins_table.php
+++ database/migrations/2024_03_12_144820_create_admins_table.php
@@ -9,35 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('phone');
-            $table->string('sex');
-            $table->date('date');
-            $table->string('district');
-            $table->string('ward');
-            $table->string('province');
             $table->string('address');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
-
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('12345678'),
-            'phone' => '0823876869',
-            'address' => '450 Ton Duc Thang',
-            'is_admin' => 1,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
     }
 
     /**
@@ -45,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 };
