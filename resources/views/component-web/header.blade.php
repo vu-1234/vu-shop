5b64289214703c98f86ac481e47cafa2515bfeb2
vu-1234 <vodaihua111@gmail.com>
2024-03-18 03:24:45 +0700

    client update

--- resources/views/component-web/header.blade.php
+++ resources/views/component-web/header.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <a class="navbar-brand" href="/">VU SHOP</a>
+    <a class="navbar-brand fw-bold" href="/">VU SHOP</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
