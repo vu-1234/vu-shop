8e4e6ddec7e84c56cb2f06d5ec1e3d4a3dee3e6e
vu-1234 <vodaihua111@gmail.com>
2024-03-26 19:27:32 +0700

    final

--- resources/views/web/cart.blade.php
+++ resources/views/web/cart.blade.php
@@ -38,7 +38,7 @@
                             @csrf
                             <div class="input-group">
                                 <button class="btn btn-outline-secondary" type="submit" name="decrement">-</button>
-                                <input type="number" class="form-control text-center"
+                                <input type="text" class="form-control text-center"
                                     value="{{ $productDetail['quantity_choose'] }}" name="quantity_choose"
                                     min="1" max="{{ $productDetail['quantity'] }}" style="width: 70px;" disabled>
                                 <button class="btn btn-outline-secondary" type="submit" name="increment">+</button>
