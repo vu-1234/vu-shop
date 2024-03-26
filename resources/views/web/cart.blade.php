d4e1b6cadc22578c7c324ab122ca615857a3567c
vu-1234 <vodaihua111@gmail.com>
2024-03-26 19:30:28 +0700

    final

--- resources/views/web/cart.blade.php
+++ resources/views/web/cart.blade.php
@@ -94,7 +94,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title fw-bold" id="checkoutModalLabel">Thông tin thanh toán</h5>
+                <h5 class="modal-title fw-bold" id="checkoutModalLabel">Thông tin giao hàng</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form id="checkoutForm" action="{{ route('web.checkout') }}" method="POST">
